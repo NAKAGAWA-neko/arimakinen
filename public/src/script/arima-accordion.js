1ccf7f9ff09d6698751862b76d545c7c357a0d17
NAKAGAWA-neko <goginzahiro@gmail.com>
2023-11-08 06:02:52 +0900

    修正

--- public/src/script/arima-accordion.js
+++ public/src/script/arima-accordion.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
   // アコーディオンの三角形とタイトルをクリックした時の動作
   $(".accordion-icon, .arima-times").click(function () {
     // このアコーディオンのコンテンツをトグル表示
-    $(this).closest(".accordion").find(".accordion-content").slideToggle(200);
+    $(this).closest(".arima-accordion").find(".accordion-content").slideToggle(200);
 
     // クリックされたアコーディオンのアイコンのクラスを切り替える
-    $(this).closest(".accordion").find(".accordion-icon").toggleClass("active");
+    $(this).closest(".arima-accordion").find(".accordion-icon").toggleClass("active");
 
     // イベントの伝播を防ぐ
     event.stopPropagation();
