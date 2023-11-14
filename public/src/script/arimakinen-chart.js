f2e458bc6824de5dc1d0745e1e3cea96e690a5a0
NAKAGAWA-neko <goginzahiro@gmail.com>
2023-11-14 14:24:09 +0900

    修正

--- public/src/script/arimakinen-chart.js
+++ public/src/script/arimakinen-chart.js
@@ -2,30 +2,30 @@
 var arimaChartCyakujyun1Config = {
   type: "bar",
   data: {
-    labels: [
-      "1番人気",
-      "2番人気",
-      "3番人気",
-      "4番人気",
-      "5番人気",
-      "6番人気",
-      "7番人気",
-      "8番人気",
-      "9番人気",
-      "10番人気",
-      "11番人気",
-      "12番人気",
-      "13番人気",
-      "14番人気",
-      "15番人気",
-      "16番人気",
-    ],
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"],
     datasets: [
       {
-        label: "頭数",
+        label: "人気馬の頭数",
         data: [26, 11, 7, 10, 0, 3, 1, 1, 4, 2, 0, 0, 0, 1, 1, 0],
-        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
-        hoverOffset: 4,
+        backgroundColor: [
+          "rgba(171,151,58, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+        ],
+        hoverOffset: 5,
       },
     ],
   },
@@ -38,6 +38,16 @@ var arimaChartCyakujyun1Config = {
         bodyFont: {
           weight: "bold", // テキストを太字に設定
         },
+        callbacks: {
+          title: function (tooltipItems) {
+            // X軸のラベルに「番人気」を追加してタイトルとして設定
+            return tooltipItems[0].label + "番人気";
+          },
+          label: function (context) {
+            // データの値に「頭」を追加してラベルとして設定
+            return context.raw + "頭";
+          },
+        },
       },
     },
     scales: {
@@ -46,10 +56,19 @@ var arimaChartCyakujyun1Config = {
           display: false,
         },
         stacked: true,
+        title: {
+          display: true, // タイトルを表示する
+          text: "人気", // 表示するテキスト
+        },
       },
       y: {
         stacked: true,
         max: 30,
+      },
+    },
+    layout: {
+      padding: {
+        bottom: 0, // 下のパディングを0に設定
       },
     },
     responsive: true,
@@ -61,30 +80,30 @@ var arimaChartCyakujyun1Config = {
 var arimaChartCyakujyun2Config = {
   type: "bar",
   data: {
-    labels: [
-      "1番人気",
-      "2番人気",
-      "3番人気",
-      "4番人気",
-      "5番人気",
-      "6番人気",
-      "7番人気",
-      "8番人気",
-      "9番人気",
-      "10番人気",
-      "11番人気",
-      "12番人気",
-      "13番人気",
-      "14番人気",
-      "15番人気",
-      "16番人気",
-    ],
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"],
     datasets: [
       {
         label: "頭数",
         data: [14, 8, 14, 4, 8, 4, 4, 2, 1, 2, 2, 1, 2, 1, 0, 0],
-        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
-        hoverOffset: 4,
+        backgroundColor: [
+          "rgba(171,151,58, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(171,151,58, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+        ],
+        hoverOffset: 5,
       },
     ],
   },
@@ -97,6 +116,16 @@ var arimaChartCyakujyun2Config = {
         bodyFont: {
           weight: "bold", // テキストを太字に設定
         },
+        callbacks: {
+          title: function (tooltipItems) {
+            // X軸のラベルに「番人気」を追加してタイトルとして設定
+            return tooltipItems[0].label + "番人気";
+          },
+          label: function (context) {
+            // データの値に「頭」を追加してラベルとして設定
+            return context.raw + "頭";
+          },
+        },
       },
     },
     scales: {
@@ -105,44 +134,54 @@ var arimaChartCyakujyun2Config = {
           display: false,
         },
         stacked: true,
+        title: {
+          display: true, // タイトルを表示する
+          text: "人気", // 表示するテキスト
+        },
       },
       y: {
         stacked: true,
         max: 30,
+      },
+    },
+    layout: {
+      padding: {
+        bottom: 0, // 下のパディングを0に設定
       },
     },
     responsive: true,
     maintainAspectRatio: false,
   },
 };
+
 // 3着
 var arimaChartCyakujyun3Config = {
   type: "bar",
   data: {
-    labels: [
-      "1番人気",
-      "2番人気",
-      "3番人気",
-      "4番人気",
-      "5番人気",
-      "6番人気",
-      "7番人気",
-      "8番人気",
-      "9番人気",
-      "10番人気",
-      "11番人気",
-      "12番人気",
-      "13番人気",
-      "14番人気",
-      "15番人気",
-      "16番人気",
-    ],
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"],
     datasets: [
       {
         label: "頭数",
         data: [6, 13, 8, 10, 1, 8, 3, 3, 3, 2, 3, 2, 2, 3, 0, 0],
-        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
-        hoverOffset: 4,
+        backgroundColor: [
+          "rgba(0,136,71, 0.7)",
+          "rgba(171,151,58, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+        ],
+        hoverOffset: 5,
       },
     ],
   },
@@ -155,6 +194,16 @@ var arimaChartCyakujyun3Config = {
         bodyFont: {
           weight: "bold", // テキストを太字に設定
         },
+        callbacks: {
+          title: function (tooltipItems) {
+            // X軸のラベルに「番人気」を追加してタイトルとして設定
+            return tooltipItems[0].label + "番人気";
+          },
+          label: function (context) {
+            // データの値に「頭」を追加してラベルとして設定
+            return context.raw + "頭";
+          },
+        },
       },
     },
     scales: {
@@ -163,10 +212,19 @@ var arimaChartCyakujyun3Config = {
           display: false,
         },
         stacked: true,
+        title: {
+          display: true, // タイトルを表示する
+          text: "人気", // 表示するテキスト
+        },
       },
       y: {
         stacked: true,
         max: 30,
+      },
+    },
+    layout: {
+      padding: {
+        bottom: 0, // 下のパディングを0に設定
       },
     },
     responsive: true,
@@ -198,8 +256,23 @@ var arimaChartJockeyConfig = {
       {
         label: "",
         data: [4, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2],
-        backgroundColor: ["rgba(232, 195, 52, 0.7)"],
-        borderColor: ["rgba(232, 195, 52, 1)"],
+        backgroundColor: [
+          "rgba(171,151,58, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+          "rgba(0,136,71, 0.7)",
+        ],
+        // borderColor: ["rgba(171,151,58, 1)"],
         borderWidth: 1,
       },
     ],
@@ -217,23 +290,33 @@ var arimaChartJockeyConfig = {
           weight: "bold", // テキストを太字に設定
         },
         callbacks: {
+          title: function (tooltipItems) {
+            // X軸のラベルに「番人気」を追加してタイトルとして設定
+            return tooltipItems[0].label + "騎手";
+          },
           label: function (context) {
-            var label = context.dataset.label || "";
-            if (label) {
-              label += ":";
-            }
-            if (context.raw !== null) {
-              label += context.raw + "回";
-            }
-            return label;
+            // データの値に「頭」を追加してラベルとして設定
+            return context.raw + "回";
           },
         },
       },
     },
     scales: {
+      x: {
+        ticks: {
+          stepSize: 1, // X軸の目盛りを整数のみにする
+        },
+      },
       y: {
         grid: {
           display: false, // 横軸のグリッド（縦線たち）を非表示
+        },
+        ticks: {
+          color: "#1e2428",
+          font: {
+            size: 14, // フォントサイズを14に設定
+            weight: "bold", // フォントを太字に設定
+          },
         },
       },
     },
--- public/src/script/arimakinen-switching.js
+++ public/src/script/arimakinen-switching.js
@@ -1,33 +1,4 @@
 window.onload = function () {
-  let element = document.querySelector(".content-change-1");
-  if (element) {
-    element.style.display = "block";
-  }
-
-  element = document.querySelector(".content-change-2");
-  if (element) {
-    element.style.display = "none";
-  }
-
-  let changeButton = document.querySelector(".changebtn");
-  if (changeButton) {
-    changeButton.addEventListener("click", function () {
-      let contentChange1 = document.querySelector(".content-change-1");
-      let contentChange2 = document.querySelector(".content-change-2");
-      if (contentChange1 && contentChange2) {
-        if (contentChange1.style.display === "block") {
-          contentChange1.style.display = "none";
-          contentChange2.style.display = "block";
-          changeButton.textContent = "年別";
-        } else {
-          contentChange1.style.display = "block";
-          contentChange2.style.display = "none";
-          changeButton.textContent = "月別";
-        }
-      }
-    });
-  }
-
   // 着順の表示切り替え
   let contentLHRChange1 = document.querySelector(".localheavyr-1");
   let contentLHRChange2 = document.querySelector(".localheavyr-2");
@@ -38,7 +9,7 @@ window.onload = function () {
   contentLHRChange2.style.display = "none";
   contentLHRChange3.style.display = "none";
 
-  let changeLHRButton = document.querySelector(".lhr-c-btn");
+  let changeLHRButton = document.querySelector(".arimakinen-content-change-btn");
   if (changeLHRButton) {
     changeLHRButton.addEventListener("click", function () {
       if (contentLHRChange1 && contentLHRChange2 && contentLHRChange3) {
@@ -47,19 +18,19 @@ window.onload = function () {
           contentLHRChange1.style.display = "none";
           contentLHRChange2.style.display = "block";
           contentLHRChange3.style.display = "none";
-          changeLHRButton.textContent = "3着馬は？";
+          changeLHRButton.textContent = "3着の人気は？";
         } else if (contentLHRChange2.style.display === "block") {
           // 2つ目のチャートを非表示、3つ目のチャートを表示
           contentLHRChange1.style.display = "none";
           contentLHRChange2.style.display = "none";
           contentLHRChange3.style.display = "block";
-          changeLHRButton.textContent = "1着馬は？";
+          changeLHRButton.textContent = "1着の人気は？";
         } else {
           // 3つ目のチャートを非表示、1つ目のチャートを表示
           contentLHRChange1.style.display = "block";
           contentLHRChange2.style.display = "none";
           contentLHRChange3.style.display = "none";
-          changeLHRButton.textContent = "2着馬は？";
+          changeLHRButton.textContent = "2着の人気は？";
         }
       }
     });
