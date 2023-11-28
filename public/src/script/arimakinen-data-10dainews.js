f7d7bc59f1057d190ed5ca39cf3d40130b97890c
NAKAGAWA-neko <goginzahiro@gmail.com>
2023-11-29 03:24:40 +0900

    feat:セルにclassなど追加

--- public/src/script/arimakinen-data-10dainews.js
+++ public/src/script/arimakinen-data-10dainews.js
@@ -1,48 +1,44 @@
 function loadAndDisplay10daiCSV(csvFilePath, containerId) {
-  // CSVファイルをフェッチしてテキストとして取得します。
   fetch(csvFilePath)
     .then((response) => response.text())
     .then((csvString) => {
-      // CSVをパースしてデータを取得します。ヘッダー付きで空行をスキップします。
       const data = Papa.parse(csvString, { header: true, skipEmptyLines: true }).data;
       let tableHTML = "<thead><tr>";
 
-      // ヘッダー行を生成します。
-      for (let header in data[0]) {
-        tableHTML += `<th>${header}</th>`;
-      }
+      // ヘッダー行の生成
+      Object.keys(data[0]).forEach((header, index) => {
+        let headerClass = `header-column-${index}`; // 列ごとにヘッダーにクラスを追加
+        tableHTML += `<th class="${headerClass}">${header}</th>`;
+      });
       tableHTML += "</tr></thead><tbody>";
 
-      // データ行を生成します。
-      for (let i = 0; i < data.length; i++) {
-        let rowHTML = "<tr>";
+      // データ行の生成
+      data.forEach((row, rowIndex) => {
+        let rowClass = `data-row-${rowIndex + 1}`; // 行ごとにクラスを追加
+        let rowHTML = `<tr class="${rowClass}">`;
         let columnIndex = 0;
-        for (let cell in data[i]) {
-          let classes = [];
-          let cellContent = data[i][cell];
 
-          // 最初の列（ランク）の処理を行います。
+        Object.values(row).forEach((cell, columnIndex) => {
+          let cellClass = `data-cell-${rowIndex + 1}-${columnIndex + 1}`; // 行と列ごとにセルにクラスを追加
+          let cellContent = cell;
+
+          // 最初の列（ランク）の特別な処理
           if (columnIndex === 0) {
-            // '位'という文字を取り除きます。
-            cellContent = cellContent.replace(/位/, "");
-            // 数字を丸で囲むためのspan要素を追加します。
+            cellContent = cell.replace(/位/, "");
             cellContent = `<span class="arimakinen-content-10dainews-news-table-circle-number">${cellContent}</span>`;
           }
 
-          rowHTML += `<td>${cellContent}</td>`;
-          columnIndex++;
-        }
+          rowHTML += `<td class="${cellClass}">${cellContent}</td>`;
+        });
+
         rowHTML += "</tr>";
         tableHTML += rowHTML;
-      }
+      });
 
       tableHTML += "</tbody>";
-      // 生成したHTMLを指定されたコンテナに設定します。
       document.getElementById(containerId).innerHTML = tableHTML;
     })
     .catch((error) => {
-      // エラーが発生した場合、エラーメッセージをコンソールに出力し、
-      // ユーザーにエラーを通知するメッセージを表示します。
       console.error("Error loading or parsing CSV: ", error);
       document.getElementById(containerId).innerHTML = "<p>Error loading data.</p>";
     });
@@ -57,25 +53,22 @@ function loadAndDisplay10daiArimaCSV(csvFilePath, containerId) {
     .then((csvString) => {
       const data = Papa.parse(csvString, { header: true, skipEmptyLines: true }).data;
 
-      // 指定された列のヘッダー名を取得します。
       const headers = Object.keys(data[0]);
-      const selectedColumns = [0, 1, 2, 3, 4, 6]; // 列のインデックス（0始まり）
+      const selectedColumns = [0, 1, 2, 3, 4, 6];
       let tableHTML = "<thead><tr>";
 
-      // 選択されたヘッダーのみを表示します。
       selectedColumns.forEach((index) => {
         tableHTML += `<th>${headers[index]}</th>`;
       });
       tableHTML += "</tr></thead><tbody>";
 
-      // 指定された行のみを処理します（最初の4行）。
       for (let i = 0; i < Math.min(data.length, 3); i++) {
         let row = data[i];
         let rowHTML = "<tr>";
         selectedColumns.forEach((index, colNumber) => {
           let cellContent = row[headers[index]];
-          // 2列目のセルに特別なクラスを適用します。
-          let cellClass = colNumber === 1 ? `column-2 cell-${i + 1}` : `column-${colNumber + 1}`;
+          // 全ての列にクラスを適用
+          let cellClass = `column-${colNumber + 1} cell-${i + 1}`;
           rowHTML += `<td class="${cellClass}">${cellContent}</td>`;
         });
         rowHTML += "</tr>";
@@ -96,43 +89,46 @@ window.loadAndDisplay10daiArimaCSV = loadAndDisplay10daiArimaCSV;
 // CSVファイルをページロード時に自動的に読み込むための関数
 function init() {
   // 国内
-  for (let year = 2022; year >= 1996; year--) {
+  for (let year = 2022; year >= 1995; year--) {
     let csvFilePath = `/src/data/kokunai-${year}.csv`;
     let containerId = `kokunai-${year}`;
-    loadAndDisplay10daiCSV(csvFilePath, containerId);
+    if (document.getElementById(containerId)) {
+      loadAndDisplay10daiCSV(csvFilePath, containerId);
+    }
   }
 
   //海外
-  for (let year = 2022; year >= 1996; year--) {
+  for (let year = 2022; year >= 1995; year--) {
     let csvFilePath = `/src/data/kaigai-${year}.csv`;
     let containerId = `kaigai-${year}`;
-    loadAndDisplay10daiCSV(csvFilePath, containerId);
+    if (document.getElementById(containerId)) {
+      loadAndDisplay10daiCSV(csvFilePath, containerId);
+    }
   }
 
   // 有馬記念結果
   // 1997年から2022年までの各年についてループを実行
-  for (let year = 2022; year >= 1997; year--) {
+  for (let year = 2022; year >= 1995; year--) {
     let csvFilePath = `/src/data/arimakinen-${year}.csv`;
     let containerId = `arimakinen-10dai-${year}`;
-    loadAndDisplay10daiArimaCSV(csvFilePath, containerId);
+    if (document.getElementById(containerId)) {
+      loadAndDisplay10daiArimaCSV(csvFilePath, containerId);
+    }
   }
-
   // 漢字と流行語
   fetch("/src/data/kanji-buzz.csv")
     .then((response) => response.text())
     .then((text) => {
       Papa.parse(text, {
         complete: function (results) {
-          // 最初の年から最後の年までループ
           for (let year = 2022; year >= 2001; year--) {
-            // CSVデータの各行に対してループ
             for (let col = 1; col <= 2; col++) {
-              let row = 2022 - year; // 行インデックスの計算
-              let elementId = `${year}-${col}`; // 要素のIDを生成
-              let cellData = results.data[row][col]; // CSVデータからセルの値を取得
-
-              // HTML要素にデータを設定
-              document.getElementById(elementId).textContent = cellData;
+              let row = 2022 - year;
+              let elementId = `o-${year}-${col}`;
+              if (document.getElementById(elementId)) {
+                let cellData = results.data[row][col];
+                document.getElementById(elementId).textContent = cellData;
+              }
             }
           }
         },
